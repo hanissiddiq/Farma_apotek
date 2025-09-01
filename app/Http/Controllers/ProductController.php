6853e35f586ef1de985faa4176e198742b492c87
Hanis siddiq <hanissiddiq10@gmail.com>
2025-09-01 09:35:25 +0700

    Add Fitur Create Products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
@@ -12,7 +15,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::all();
+        return view('admin.products.index', [ 'products' => $products ]);
     }
 
     /**
@@ -20,7 +24,10 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        return view('admin.products.create', [
+            'categories' => $categories
+        ]);
     }
 
     /**
@@ -28,7 +35,40 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+
         //
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'about' => 'required|string',
+            'category_id' => 'required|integer',
+            'price' => 'required|integer',
+            'photo' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            if($request->hasFile('photo')){
+
+                $photoPath = $request->file('photo')->store('product_photos', 'public');
+                $validated['photo'] = $photoPath;
+            }
+            $validated['slug'] = Str::slug($request->name);
+            $newProduct = Product::create($validated);
+
+            //ketika berhasil simpan
+            DB::commit();
+
+            return redirect()->route('admin.products.index')->with('success', 'Product created successfully.');
+        } catch (\Exception $e) {
+            //ketika gagal simpan
+            DB::rollBack();
+            $error = ValidationException::withMessages([
+                'system_error' => ['System Error!',$e->getMessage()],
+            ]);
+            throw $error;
+            // return redirect()->back()->with('error', 'Failed to create category.');
+        }
     }
 
     /**
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,3 +1,45 @@
-// file ini unutk menampilkan daftar produk
-// nantinya akan ada fitur CRUD untuk produk
-// seperti menambah, mengedit, dan menghapus produk
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex flex-row justify-between items-center w-full">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Manage Products') }}
+            </h2>
+            <a href="{{ route('admin.products.create') }}" class="px-5 py-3 rounded-full text-white bg-indigo-500 hover:bg-indigo-600">Add Product</a>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6  lg:px-8">
+            <div class="bg-white flex flex-col gap-y-5 dark:bg-gray-800 p-10 overflow-hidden shadow-sm sm:rounded-lg">
+
+                @forelse ($products as $product )
+                <div class="item card flex flex-row justify-between items-center">
+                    <div class="flex flex-row items-center gap-x-3">
+                    <img src="{{ Storage::url($product->photo) }}" alt="" class="w-[50px] h-[50px] object-cover">
+                    <div>
+                        <h3 class="text-2xl font-bold text-gray-900 dark:text-white">
+                        {{ $product->name }}
+                        </h3>
+                        <p class="text-base text-slate-500">{{ 'Rp ' . number_format($product->price, 0, ',', '.') }}</p>
+
+                    </div>
+                    </div>
+                    <div class="flex flex-row items-center gap-x-2">
+
+                        <a href="{{ route('admin.products.edit', $product) }}" class="px-5 py-3 rounded-full text-white bg-indigo-500 hover:bg-indigo-600">Edit</a>
+                        <form method="POST" action="{{ route('admin.products.destroy', $product) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="px-5 py-3 rounded-full text-white bg-red-500 hover:bg-red-600">Delete</button>
+                        </form>
+                    </div>
+                </div>
+
+                @empty
+
+                @endforelse
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
